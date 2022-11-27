c981f806c5516d6f000ecdec1731cbd0d8996d20
Raymond Xu <raynix@gmail.com>
2022-11-28 09:52:44 +1100

    Added CSV to stdout and DEV option

--- dictionaryScript.py
+++ dictionaryScript.py
@@ -1,13 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
-#alph = "abcdefghijklmnopqrstuvwxyz"
-alph = "a"
-
-def crawl():
+def crawl(alph):
     for letter in alph:
         # this can run a long time its helpfull to know which letter its on
         logger.info(f"processing letter {letter}...")
@@ -21,10 +19,17 @@ def crawl():
             cut = len(word) + len(pos) + 4 # calulate how much word and pos take up
             definition = entries.getText()[cut:] # cut that from the total sting to get definition
             yield word, pos, definition
-        #
-        #
-        #            DO what you need here
-        # this loop will run through all the words in the
-        # english dictionary, seperating them by word, pos and definition
-        #
-        #
+
+#            DO what you need here
+# this loop will run through all the words in the
+# english dictionary, seperating them by word, pos and definition
+#
+# example to print out CSV to stdout
+# use DEV=true to test small amount of words
+if __name__ == '__main__':
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    if os.environ.get('DEV'):
+        alph = "x"
+    print("word,pos,definition")
+    for word, pos, definition in crawl(alph):
+        print(f"{word},{pos},\"{definition}\"")
