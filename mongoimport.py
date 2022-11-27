4b013fb6a0c36afb93fae3691ba4328ee73a693f
Raymond Xu <raynix@gmail.com>
2022-11-28 10:02:57 +1100

    Added DEV option to mongoimport

--- mongoimport.py
+++ mongoimport.py
@@ -10,40 +10,27 @@ class Word(Document):
     word = fields.StringField(primary=True)
     definition = fields.ListField(default=[])
 
-    @classmethod
-    def find_or_create(cls, word, pos, definition):
-        w = cls.objects(word=word).first()
-        if w is None:
-            w = Word(
-                word = word,
-                definition = [
-                    { "pos": pos, "meaning": definition }
-                ]
-            )
-        else:
-            w.definition.append({ "pos": pos, "meaning": definition })
-        w.save()
-        logger.info(f"Added word {word}")
-        return w
-
-
-def mongo_import(host, db, username, password):
+def mongo_import(host, db, username, password, alph):
     mongoengine.connect(db=db, host=host, username=username, password=password)
     previous = None
-    for word, pos, definition in crawl():
+    for word, pos, definition in crawl(alph):
         if not previous:
+            logger.info(f"Starting...")
             previous = Word()
             previous.word = word
-            previous.definition.append({ "pos": pos, "meaning": definition })
         elif previous.word != word:
+            logger.info(f"Moving on to new word {word}...")
             previous.save()
             previous = Word()
             previous.word = word
-            previous.definition.append({ "pos": pos, "meaning": definition })
-        else:
-            previous.definition.append({ "pos": pos, "meaning": definition })
+        logger.info(f"Adding definition for word {word}")
+        previous.definition.append({ "pos": pos, "meaning": definition })
     if previous:
         previous.save()
 
+# sample MongoDB connection settings.
 if __name__ == '__main__':
-    mongo_import('192.168.1.51', 'wordsquad', 'wordsquad', os.environ.get('MONGO_PASS'))
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    if os.environ.get('DEV'):
+        alph = "x"
+    mongo_import('192.168.1.51', 'wordsquad', 'wordsquad', os.environ.get('MONGO_PASS'), alph)
